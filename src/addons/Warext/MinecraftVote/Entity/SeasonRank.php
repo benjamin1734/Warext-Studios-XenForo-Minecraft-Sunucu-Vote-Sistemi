88495ab703c0f7bc04b3c3caf174f74033916d43
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 11:36:25 +0300

    Sezon sıralama geçmişi entity yapısını ekle

---
+++ src/addons/Warext/MinecraftVote/Entity/SeasonRank.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Warext\MinecraftVote\Entity;
+
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Structure;
+
+class SeasonRank extends Entity
+{
+    public static function getStructure(Structure $structure): Structure
+    {
+        $structure->table = 'xf_warext_mc_season_rank';
+        $structure->shortName = 'Warext\MinecraftVote:SeasonRank';
+        $structure->primaryKey = ['season_id', 'server_id'];
+        $structure->columns = [
+            'season_id' => ['type' => self::UINT, 'required' => true],
+            'server_id' => ['type' => self::UINT, 'required' => true],
+            'rank' => ['type' => self::UINT, 'default' => 0],
+            'vote_count' => ['type' => self::UINT, 'default' => 0],
+            'unique_voters' => ['type' => self::UINT, 'default' => 0],
+            'uptime_bp' => ['type' => self::UINT, 'default' => 0],
+            'peak_players' => ['type' => self::UINT, 'default' => 0],
+            'season_score_bp' => ['type' => self::UINT, 'default' => 0],
+            'snapshot_date' => ['type' => self::UINT, 'default' => 0]
+        ];
+        $structure->relations = [
+            'Season' => [
+                'entity' => 'Warext\MinecraftVote:Season',
+                'type' => self::TO_ONE,
+                'conditions' => 'season_id',
+                'primary' => true
+            ],
+            'Server' => [
+                'entity' => 'Warext\MinecraftVote:Server',
+                'type' => self::TO_ONE,
+                'conditions' => 'server_id',
+                'primary' => true
+            ]
+        ];
+
+        return $structure;
+    }
+}
