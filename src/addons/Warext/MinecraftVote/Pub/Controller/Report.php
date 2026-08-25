ddcd3286dfe39f289c66fd7ecd566584a05af811
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 15:52:28 +0300

    v0.13.0

---
+++ src/addons/Warext/MinecraftVote/Pub/Controller/Report.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Warext\MinecraftVote\Pub\Controller;
+
+use XF\Mvc\ParameterBag;
+use XF\Pub\Controller\AbstractController;
+
+class Report extends AbstractController
+{
+    public function actionIndex(ParameterBag $params)
+    {
+        $visitor = \XF::visitor();
+        if (!$visitor->user_id)
+        {
+            return $this->noPermission();
+        }
+
+        $server = $this->em()->find('Warext\MinecraftVote:Server', (int)$params->server_id);
+        if (!$server || $server->state !== 'active')
+        {
+            throw $this->exception($this->notFound());
+        }
+
+        if ($server->owner_user_id === $visitor->user_id)
+        {
+            return $this->noPermission();
+        }
+
+        if ($this->isPost())
+        {
+            $this->assertPostOnly();
+            $reason = $this->filter('reason', 'str');
+            $message = $this->filter('message', 'str');
+
+            try
+            {
+                $creator = $this->service('Warext\MinecraftVote:Report\Creator', $server, $visitor);
+                $creator->setData($reason, $message);
+                $creator->save();
+            }
+            catch (\XF\PrintableException $e)
+            {
+                return $this->error($e->getMessage(), 400);
+            }
+
+            return $this->redirect(
+                $this->buildLink('sunucular/detay', $server),
+                'Raporunuz moderasyon ekibine gönderildi.'
+            );
+        }
+
+        return $this->view('Warext\MinecraftVote:Report\Create', 'warext_mc_report_create', [
+            'server' => $server
+        ]);
+    }
+}
