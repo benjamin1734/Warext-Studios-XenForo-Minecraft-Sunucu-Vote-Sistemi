56ff560c21542769f02d4d283a7619a96c334d49
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 14:16:05 +0300

    Sunucu detay görünürlüğünü ekip yetkileriyle eşleştir

---
+++ src/addons/Warext/MinecraftVote/Pub/Controller/Detail.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Warext\MinecraftVote\Pub\Controller;
+
+use Warext\MinecraftVote\Entity\Server;
+use XF\Mvc\ParameterBag;
+use XF\Pub\Controller\AbstractController;
+
+class Detail extends AbstractController
+{
+    public function actionIndex(ParameterBag $params)
+    {
+        $server = $this->assertViewableServer((int)$params->server_id);
+
+        if ($server->state === 'active')
+        {
+            $this->repository('Warext\MinecraftVote:Server')->incrementViewCount($server);
+        }
+
+        $achievements = $this->repository('Warext\MinecraftVote:Achievement')
+            ->findForServer($server->server_id)
+            ->fetch();
+
+        return $this->view('Warext\MinecraftVote:Server\View', 'warext_mc_server_view', [
+            'server' => $server,
+            'achievements' => $achievements
+        ]);
+    }
+
+    protected function assertViewableServer(int $serverId): Server
+    {
+        $server = $this->em()->find('Warext\MinecraftVote:Server', $serverId, ['Owner']);
+        if (!$server)
+        {
+            throw $this->exception($this->notFound());
+        }
+
+        if ($server->state === 'active')
+        {
+            return $server;
+        }
+
+        $visitor = \XF::visitor();
+        if (!$visitor->user_id)
+        {
+            throw $this->exception($this->notFound());
+        }
+
+        if ($server->is_owner
+            || $server->can_edit
+            || $server->can_publish_updates
+            || $server->can_view_stats
+            || $server->can_manage_votifier
+            || $server->can_manage_reviews)
+        {
+            return $server;
+        }
+
+        throw $this->exception($this->notFound());
+    }
+}
