32c23fa2e0c760dfcccf62a53b3270c8c2ba49c7
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 10:23:26 +0300

    Sunucu ekip entity katmanını ekle

---
+++ src/addons/Warext/MinecraftVote/Entity/ServerTeam.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Warext\MinecraftVote\Entity;
+
+use XF\Mvc\Entity\Entity;
+use XF\Mvc\Entity\Structure;
+
+class ServerTeam extends Entity
+{
+    public static function getStructure(Structure $structure): Structure
+    {
+        $structure->table = 'xf_warext_mc_server_team';
+        $structure->shortName = 'Warext\MinecraftVote:ServerTeam';
+        $structure->primaryKey = ['server_id', 'user_id'];
+        $structure->columns = [
+            'server_id' => ['type' => self::UINT, 'required' => true],
+            'user_id' => ['type' => self::UINT, 'required' => true],
+            'role' => ['type' => self::STR, 'maxLength' => 20, 'default' => 'member'],
+            'permissions' => ['type' => self::SERIALIZED_ARRAY, 'default' => []],
+            'joined_date' => ['type' => self::UINT, 'default' => 0]
+        ];
+        $structure->relations = [
+            'Server' => [
+                'entity' => 'Warext\MinecraftVote:Server',
+                'type' => self::TO_ONE,
+                'conditions' => 'server_id',
+                'primary' => true
+            ],
+            'User' => [
+                'entity' => 'XF:User',
+                'type' => self::TO_ONE,
+                'conditions' => 'user_id',
+                'primary' => true
+            ]
+        ];
+
+        return $structure;
+    }
+
+    protected function _preSave(): void
+    {
+        if (!$this->joined_date)
+        {
+            $this->joined_date = \XF::$time;
+        }
+    }
+}
