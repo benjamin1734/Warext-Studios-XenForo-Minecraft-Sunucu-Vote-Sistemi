b711a25eaaf01d6e82441cd3cda6bff9a4538d87
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 12:48:54 +0300

    Alert handler method imzalarını XenForo 2.3 ile uyumlu yap

--- src/addons/Warext/MinecraftVote/Alert/ServerUpdate.php
+++ src/addons/Warext/MinecraftVote/Alert/ServerUpdate.php
@@ -8,30 +8,30 @@ use XF\Mvc\Entity\Entity;
 
 class ServerUpdate extends AbstractHandler
 {
-    public function getEntityWith()
+    public function getEntityWith(): array
     {
         return ['Server', 'User'];
     }
 
-    public function canViewContent(Entity $entity, &$error = null)
+    public function canViewContent(Entity $entity, &$error = null): bool
     {
         return $entity->state === 'visible'
             && $entity->Server
             && $entity->Server->state === 'active';
     }
 
-    public function canViewAlert(UserAlert $alert, &$error = null)
+    public function canViewAlert(UserAlert $alert, &$error = null): bool
     {
         $content = $alert->Content;
         return $content ? $this->canViewContent($content, $error) : false;
     }
 
-    public function getOptOutActions()
+    public function getOptOutActions(): array
     {
         return ['publish'];
     }
 
-    public function getOptOutDisplayOrder()
+    public function getOptOutDisplayOrder(): int
     {
         return 500;
     }
