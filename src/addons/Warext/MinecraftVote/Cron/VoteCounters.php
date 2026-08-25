00a7cdff0b8e974a152364a33a08351908dea603
benjamin1734 <muzafferbatingurnes@gmail.com>
2026-08-25 11:17:45 +0300

    Oy sayaç bakım cronunu ekle

---
+++ src/addons/Warext/MinecraftVote/Cron/VoteCounters.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Warext\MinecraftVote\Cron;
+
+class VoteCounters
+{
+    public static function run(): void
+    {
+        $timeZoneId = (string)(\XF::options()->guestTimeZone ?? 'UTC');
+
+        try
+        {
+            $timeZone = new \DateTimeZone($timeZoneId ?: 'UTC');
+        }
+        catch (\Throwable)
+        {
+            $timeZone = new \DateTimeZone('UTC');
+        }
+
+        $now = (new \DateTimeImmutable('@' . \XF::$time))->setTimezone($timeZone);
+        $dayStart = $now->setTime(0, 0)->getTimestamp();
+        $monthStart = $now->modify('first day of this month')->setTime(0, 0)->getTimestamp();
+
+        $rows = \XF::db()->fetchAll(
+            "SELECT
+                server_id,
+                COUNT(*) AS vote_count_total,
+                SUM(vote_date >= ?) AS vote_count_month,
+                SUM(vote_date >= ?) AS vote_count_today
+            FROM xf_warext_mc_vote
+            WHERE status <> 'rejected'
+            GROUP BY server_id",
+            [$monthStart, $dayStart]
+        );
+
+        \XF::db()->update('xf_warext_mc_server', [
+            'vote_count_total' => 0,
+            'vote_count_month' => 0,
+            'vote_count_today' => 0
+        ]);
+
+        foreach ($rows as $row)
+        {
+            \XF::db()->update('xf_warext_mc_server', [
+                'vote_count_total' => (int)$row['vote_count_total'],
+                'vote_count_month' => (int)$row['vote_count_month'],
+                'vote_count_today' => (int)$row['vote_count_today']
+            ], 'server_id = ?', (int)$row['server_id']);
+        }
+    }
+}
